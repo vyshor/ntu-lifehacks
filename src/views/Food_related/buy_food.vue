beae87dcf4ce1d6369d0047820cab8d273b78ce3
vyshor <vyshornehc@gmail.com>
2019-02-03 13:02:29 +0800

    Added CART

--- src/views/Food_related/buy_food.vue
+++ src/views/Food_related/buy_food.vue
@@ -32,15 +32,15 @@
 
                                     </v-flex>
                                     <v-flex xs12 sm12 d-flex>
-                                        <v-data-table v-for="storee in canteen"
-                                                      :headers="headers"
-                                                      :items="conversionToArray(storee)"
-                                                      class="elevation-1"
-                                                      v-if="selected_store[canteen.canteenName] === storee.storeName"
-                                                      :rows-per-page-items="[4,8]"
-                                                      style="height: 5%; width: 70%;"
-                                        >
-                                            <template slot="items" slot-scope="props">
+                                                    <v-data-table v-for="storee in canteen"
+                                                                  :headers="headers"
+                                                                  :items="conversionToArray(storee)"
+                                                                  class="elevation-1"
+                                                                  v-if="selected_store[canteen.canteenName] === storee.storeName"
+                                                                  :rows-per-page-items="[4,8]"
+                                                                  style="height: 5%; width: 70%;"
+                                                    >
+                                                        <template slot="items" slot-scope="props">
                                                 <td style="margin: 0; padding: 0;" class="mx-1 px-1">{{ props.item.name
                                                     }}
                                                 </td>
@@ -75,7 +75,7 @@
                                                             <v-btn fab color="info" dark small>
                                                                 <v-icon
                                                                         small
-                                                                        @click="addToCart(props.item.name, canteen.canteenName, storee.storeName)"
+                                                                        @click="addToCart(props.item.name, canteen.canteenName, storee.storeName, props.item.price)"
                                                                         center
                                                                 >
                                                                     fa-cart-plus
@@ -110,13 +110,62 @@
                             class="mb-5"
                             color="grey lighten-1"
                             height="200px"
-                    ></v-card>
+                    >
+                        <v-flex xs12 sm12 d-flex>
+                            <v-data-table
+                                          :headers="headers_cart"
+                                          :items="cart"
+                                          class="elevation-1"
+                                          :rows-per-page-items="[10,20]"
+                                          style="height: 5%; width: 70%;"
+                            >
+                                <template slot="items" slot-scope="props">
+                                    <td style="margin: 0; padding: 0;" class="mx-1 px-1">{{ props.item.location
+                                        }}
+                                    </td>
+                                    <td class="text-xs-left" style="margin: 0; padding: 0;">{{
+                                        props.item.store }}
+                                    </td>
+                                    <td class="text-xs-left" style="margin: 0; padding: 0;">{{
+                                        props.item.dish }}
+                                    </td>
+                                    <td class="text-xs-left" style="margin: 0; padding: 0;">{{
+                                        props.item.price }}
+                                    </td>
+                                    <td class="text-xs-left" style="margin: 0; padding: 0;">
+                                        <v-text-field
+                                                type="number"
+                                                min="1"
+                                                step="1"
+                                                v-model="props.item.qty">
+                                            <!--:ref="props.item.name"-->
+                                        </v-text-field>
+                                    </td>
+                                    <td style="margin: 0; padding: 0; width: 7%;" class="mx-0 px-0">
+                                        <v-btn fab dark small color="indigo">
+                                            <v-icon dark small
+                                                    @click="removeFromCart(props.item)">cross
+                                            </v-icon>
+                                        </v-btn>
+                                    </td>
+                                </template>
+                            </v-data-table>
+                        </v-flex>
+                    </v-card>
+
+                    <p>Total Cost: {{ this.total_cost }}</p>
 
                     <v-btn
                             color="primary"
                             @click="e1 = 3"
                     >
-                        Continue
+                        Proceed to payment
+                    </v-btn>
+                    <v-btn
+                            color="success"
+                            @click="e1 = 1"
+                    >
+                        Back
                     </v-btn>
 
                     <v-btn flat>Cancel</v-btn>
@@ -133,7 +182,7 @@
                             color="primary"
                             @click="e1 = 1"
                     >
-                        Continue
+                        Confirm
                     </v-btn>
 
                     <v-btn flat>Cancel</v-btn>
@@ -177,7 +226,35 @@
                         align: 'left'
                     }
                 ],
-                cart: {}
+                headers_cart: [
+                    {
+                        text: 'Location',
+                        align: 'left',
+                        sortable: true,
+                        value: 'location'
+                    },
+                    {
+                        text: 'Store',
+                        align: 'left',
+                        sortable: true,
+                        value: 'store'
+                    },
+                    {
+                        text: 'Selection',
+                        value: 'dish',
+                        align: 'left'
+                    },
+                    {
+                        text: 'Price ($)', value: 'price',
+                        align: 'left'
+                    },
+                    {
+                        text: 'Qty', value: 'qty',
+                        align: 'left'
+                    }
+
+                ],
+                cart: []
             }
         },
         created() {
@@ -256,14 +333,38 @@
                 }
                 // this.menu_quantity[ref]--;
             },
-            addToCart: function (ref, canteenName, storeName) {
+            addToCart: function (ref, canteenName, storeName, price) {
                 const val = this.menu_quantity[ref];
-                console.log(canteenName, storeName, val);
+                console.log(canteenName, storeName, ref, price, val);
+                this.cart.push({
+                    location: canteenName,
+                    store: storeName,
+                    dish: ref,
+                    price: price,
+                    qty: val,
+                    total: 0
+                })
             },
+            removeFromCart: function(item)  {
+                let index = this.cart.indexOf(item);
+                this.cart.splice(index, 1);
+            },
+            saveOrderToDatabase: function() {
+                
+            }
         },
         updated() {
         },
-        computed: {}
+        computed: {
+            total_cost: function() {
+                let total = 0.;
+                for (let order of this.cart) {
+                    total += parseFloat(order.price) * parseInt(order.qty);
+                    console.log(order.price, order.qty);
+                }
+                return total;
+            }
+        }
 
     }
 </script>
