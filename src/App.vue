b231f05ac5ac2f800418f1001aeb25f73e8a4895
vyshor <vyshornehc@gmail.com>
2019-02-04 15:05:20 +0800

    Fix merge conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
-  <v-app class="grey lighten-4">
-    <Navbar></Navbar>
-    <v-content class="ma-0">
-      <router-view></router-view>
-    </v-content>
-      <BotBar></BotBar>
+    <v-app class="grey lighten-4">
+        <Navbar></Navbar>
+        <v-content class="ma-0">
+            <router-view></router-view>
+        </v-content>
+        <BotBar></BotBar>
     </v-app>
 </template>
 
 <script>
     import BotBar from "@/components/BotBar"
-import Navbar from "@/components/Navbar";
-import Botbar from "@/components/BotBar";
+    import Navbar from "@/components/Navbar";
+    import Botbar from "@/components/BotBar";
 
-export default {
-  name: "App",
-<<<<<<< HEAD
-  components: { Navbar, BotBar },
-=======
-  components: { Navbar, Botbar },
->>>>>>> 5cfb7543721919a93ce4836bf4b67e7328250105
-  data() {
-    return {
-      //
+    export default {
+        name: "App",
+        components: {Navbar, BotBar},
+        data() {
+            return {
+                //
+            };
+        }
     };
-  }
-};
 </script>
